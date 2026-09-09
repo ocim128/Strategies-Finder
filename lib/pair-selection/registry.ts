bbd597a2d0b5fb70c36dd3ec3fd60db9cd81b909
Mickey <user@DESKTOP-NE8ADSU>
2026-09-09 13:41:47 +0700

    Remove unsupported pair selection rules

--- lib/pair-selection/registry.ts
+++ lib/pair-selection/registry.ts
@@ -3,9 +3,6 @@ import { adverse_channel_bounce_proximity } from "./adverse_channel_bounce_proxi
 import { adverse_increment_streak_rebound } from "./adverse_increment_streak_rebound";
 import { autocorr_penalized_drift } from "./autocorr_penalized_drift";
 import { autoregressive_persistence_scaled_drift } from "./autoregressive_persistence_scaled_drift";
-import { bars_since_directional_extreme } from "./bars_since_directional_extreme";
-import { best_bar_back_loading } from "./best_bar_back_loading";
-import { best_evidence_drift } from "./best_evidence_drift";
 import { base_cohort_excess_momentum } from "./base_cohort_excess_momentum";
 import { base_cohort_direction_purity_weight } from "./base_cohort_direction_purity_weight";
 import { base_theme_deduplicated_leader } from "./base_theme_deduplicated_leader";
@@ -16,7 +13,6 @@ import { canary_leader_gate } from "./canary_leader_gate";
 import { cleanliness_lottery_pick } from "./cleanliness_lottery_pick";
 import { cohort_alignment_regime_switch } from "./cohort_alignment_regime_switch";
 import { cohort_normalized_directional_rank } from "./cohort_normalized_directional_rank";
-import { cohort_record_share_weight } from "./cohort_record_share_weight";
 import { cohort_vice_champion } from "./cohort_vice_champion";
 import { cointegrated_base_breakout_ratio } from "./cointegrated_base_breakout_ratio";
 import { concordant_drift_velocity_product } from "./concordant_drift_velocity_product";
@@ -45,15 +41,11 @@ import { directional_zscore_velocity_expansion } from "./directional_zscore_velo
 import { discrete_spread_path_acceleration } from "./discrete_spread_path_acceleration";
 import { direction_asymmetry_test } from "./direction_asymmetry_test";
 import { dip_share_regime_switch } from "./dip_share_regime_switch";
-import { disjoint_thirds_minimax } from "./disjoint_thirds_minimax";
 import { dual_cohort_champion } from "./dual_cohort_champion";
-import { dual_engine_agreement_veto } from "./dual_engine_agreement_veto";
 import { dual_visibility_percentile } from "./dual_visibility_percentile";
 import { dispersion_conditioned_horizon_switch } from "./dispersion_conditioned_horizon_switch";
 import { divergent_leg_momentum_concordance } from "./divergent_leg_momentum_concordance";
 import { donchian_range_expansion_momentum } from "./donchian_range_expansion_momentum";
-import { dow_sampled_momentum } from "./dow_sampled_momentum";
-import { directional_sign_fraction } from "./directional_sign_fraction";
 import { econometric_forward_return_projection } from "./econometric_forward_return_projection";
 import { efficiency_maturity_rank_product } from "./efficiency_maturity_rank_product";
 import { efficiency_skew_regime_switch } from "./efficiency_skew_regime_switch";
@@ -66,10 +58,7 @@ import { event_horizon_conflict_regime_switch } from "./event_horizon_conflict_r
 import { elite_decile_fnv_pick } from "./elite_decile_fnv_pick";
 import { escape_hatch_union_pick } from "./escape_hatch_union_pick";
 import { experience_payoff_regime_switch } from "./experience_payoff_regime_switch";
-import { fat_tail_veto } from "./fat_tail_veto";
 import { fast_halflife_adverse_displacement } from "./fast_halflife_adverse_displacement";
-import { follow_through_weighted_drift } from "./follow_through_weighted_drift";
-import { golden_cross_recency } from "./golden_cross_recency";
 import { fresh_trend_emergence_ratio } from "./fresh_trend_emergence_ratio";
 import { fresh_pair_momentum_weight } from "./fresh_pair_momentum_weight";
 import { global_podium_rank_pick } from "./global_podium_rank_pick";
@@ -78,18 +67,12 @@ import { horizon_conflict_lottery_pick } from "./horizon_conflict_lottery_pick";
 import { institutional_wave_persistence_spread } from "./institutional_wave_persistence_spread";
 import { interday_persistence_scaled_momentum } from "./interday_persistence_scaled_momentum";
 import { intermediate_directional_spread_momentum } from "./intermediate_directional_spread_momentum";
-import { jump_discounted_drift } from "./jump_discounted_drift";
 import { laplace_bayesian_odds_ratio } from "./laplace_bayesian_odds_ratio";
 import { laplace_log_odds_win_rate } from "./laplace_log_odds_win_rate";
 import { laplace_uniform_win_rate } from "./laplace_uniform_win_rate";
-import { leg_correlation_independence } from "./leg_correlation_independence";
-import { leave_best_out_drift } from "./leave_best_out_drift";
-import { leverage_healing_weight } from "./leverage_healing_weight";
 import { linear_determination_scaled_drift } from "./linear_determination_scaled_drift";
-import { memory_decay_split } from "./memory_decay_split";
 import { low_efficiency_exhaustion_spring } from "./low_efficiency_exhaustion_spring";
 import { multi_horizon_directional_concordance } from "./multi_horizon_directional_concordance";
-import { median_increment_drift } from "./median_increment_drift";
 import { minority_direction_momentum_weight } from "./minority_direction_momentum_weight";
 import { momentum_maturity_rank_product } from "./momentum_maturity_rank_product";
 import { momentum_percentile_target } from "./momentum_percentile_target";
@@ -100,29 +83,21 @@ import { one_leg_theme_drift } from "./one_leg_theme_drift";
 import { pareto_undominated_pick } from "./pareto_undominated_pick";
 import { pre_event_vol_scaled_momentum } from "./pre_event_vol_scaled_momentum";
 import { persistence_regime_switch } from "./persistence_regime_switch";
-import { peak_proximity_entry } from "./peak_proximity_entry";
 import { pool_skew_regime_switch } from "./pool_skew_regime_switch";
 import { pullback_discounted_directional_momentum } from "./pullback_discounted_directional_momentum";
-import { quadratic_path_curvature } from "./quadratic_path_curvature";
 import { quote_cohort_excess_momentum } from "./quote_cohort_excess_momentum";
-import { prospect_ledger } from "./prospect_ledger";
 import { rank_neighborhood_smoothed_pick } from "./rank_neighborhood_smoothed_pick";
-import { range_freshness_orientation } from "./range_freshness_orientation";
-import { record_print_rate_momentum } from "./record_print_rate_momentum";
 import { reference_alphabetical, reference_loudest_atr } from "./references";
 import { short_horizon_directional_momentum } from "./short_horizon_directional_momentum";
 import { short_to_intermediate_reversion_gap } from "./short_to_intermediate_reversion_gap";
 import { scale_diffusion_residual } from "./scale_diffusion_residual";
 import { scale_ambiguity_veto } from "./scale_ambiguity_veto";
 import { slow_breadth_regime_switch } from "./slow_breadth_regime_switch";
-import { skew_weighted_drift } from "./skew_weighted_drift";
 import { slope_return_rank_gap } from "./slope_return_rank_gap";
 import { spread_efficiency_acceleration_ratio } from "./spread_efficiency_acceleration_ratio";
 import { spread_path_information_ratio } from "./spread_path_information_ratio";
 import { spread_range_asymmetry_odds_ratio } from "./spread_range_asymmetry_odds_ratio";
 import { trend_to_noise_volatility_ratio } from "./trend_to_noise_volatility_ratio";
-import { trend_t_statistic } from "./trend_t_statistic";
-import { trendline_residual_pullback } from "./trendline_residual_pullback";
 import { tail_excluded_fnv_pick } from "./tail_excluded_fnv_pick";
 import { theme_tightness_weight } from "./theme_tightness_weight";
 import { theme_decisiveness_regime_switch } from "./theme_decisiveness_regime_switch";
@@ -134,11 +109,8 @@ import { volatility_clustering_scaled_momentum } from "./volatility_clustering_s
 import { volatility_impulse_scaled_momentum } from "./volatility_impulse_scaled_momentum";
 import { volatility_scaled_adverse_median_reversion } from "./volatility_scaled_adverse_median_reversion";
 import { wilson_lower_bound_win_rate } from "./wilson_lower_bound_win_rate";
-import { worst_bar_front_loading } from "./worst_bar_front_loading";
 import { worst_horizon_momentum } from "./worst_horizon_momentum";
 import { unanimous_trend_lottery_pick } from "./unanimous_trend_lottery_pick";
-import { stochastic_half_upgrade } from "./stochastic_half_upgrade";
-import { uptime_momentum } from "./uptime_momentum";
 import { winner_tightness_regime_switch } from "./winner_tightness_regime_switch";
 import type { PairSelectionRule } from "./types";
 
@@ -147,9 +119,6 @@ const pairSelectionRuleDefinitions = [
     adverse_increment_streak_rebound,
     autocorr_penalized_drift,
     autoregressive_persistence_scaled_drift,
-    bars_since_directional_extreme,
-    best_bar_back_loading,
-    best_evidence_drift,
     base_cohort_excess_momentum,
     base_cohort_direction_purity_weight,
     base_theme_deduplicated_leader,
@@ -160,7 +129,6 @@ const pairSelectionRuleDefinitions = [
     cleanliness_lottery_pick,
     cohort_alignment_regime_switch,
     cohort_normalized_directional_rank,
-    cohort_record_share_weight,
     cohort_vice_champion,
     cointegrated_base_breakout_ratio,
     concordant_drift_velocity_product,
@@ -189,15 +157,11 @@ const pairSelectionRuleDefinitions = [
     discrete_spread_path_acceleration,
     direction_asymmetry_test,
     dip_share_regime_switch,
-    disjoint_thirds_minimax,
     dual_cohort_champion,
-    dual_engine_agreement_veto,
     dual_visibility_percentile,
     dispersion_conditioned_horizon_switch,
     divergent_leg_momentum_concordance,
     donchian_range_expansion_momentum,
-    dow_sampled_momentum,
-    directional_sign_fraction,
     econometric_forward_return_projection,
     efficiency_maturity_rank_product,
     efficiency_skew_regime_switch,
@@ -210,10 +174,7 @@ const pairSelectionRuleDefinitions = [
     elite_decile_fnv_pick,
     escape_hatch_union_pick,
     experience_payoff_regime_switch,
-    fat_tail_veto,
     fast_halflife_adverse_displacement,
-    follow_through_weighted_drift,
-    golden_cross_recency,
     fresh_trend_emergence_ratio,
     fresh_pair_momentum_weight,
     global_podium_rank_pick,
@@ -222,18 +183,12 @@ const pairSelectionRuleDefinitions = [
     institutional_wave_persistence_spread,
     interday_persistence_scaled_momentum,
     intermediate_directional_spread_momentum,
-    jump_discounted_drift,
     laplace_bayesian_odds_ratio,
     laplace_log_odds_win_rate,
     laplace_uniform_win_rate,
-    leg_correlation_independence,
-    leave_best_out_drift,
-    leverage_healing_weight,
     linear_determination_scaled_drift,
-    memory_decay_split,
     low_efficiency_exhaustion_spring,
     multi_horizon_directional_concordance,
-    median_increment_drift,
     minority_direction_momentum_weight,
     momentum_maturity_rank_product,
     momentum_percentile_target,
@@ -244,15 +199,10 @@ const pairSelectionRuleDefinitions = [
     pareto_undominated_pick,
     pre_event_vol_scaled_momentum,
     persistence_regime_switch,
-    peak_proximity_entry,
     pool_skew_regime_switch,
     pullback_discounted_directional_momentum,
-    quadratic_path_curvature,
     quote_cohort_excess_momentum,
-    prospect_ledger,
     rank_neighborhood_smoothed_pick,
-    range_freshness_orientation,
-    record_print_rate_momentum,
     reference_alphabetical,
     reference_loudest_atr,
     short_horizon_directional_momentum,
@@ -260,14 +210,11 @@ const pairSelectionRuleDefinitions = [
     scale_diffusion_residual,
     scale_ambiguity_veto,
     slow_breadth_regime_switch,
-    skew_weighted_drift,
     slope_return_rank_gap,
     spread_efficiency_acceleration_ratio,
     spread_path_information_ratio,
     spread_range_asymmetry_odds_ratio,
     trend_to_noise_volatility_ratio,
-    trend_t_statistic,
-    trendline_residual_pullback,
     tail_excluded_fnv_pick,
     theme_tightness_weight,
     theme_decisiveness_regime_switch,
@@ -279,11 +226,8 @@ const pairSelectionRuleDefinitions = [
     volatility_impulse_scaled_momentum,
     volatility_scaled_adverse_median_reversion,
     wilson_lower_bound_win_rate,
-    worst_bar_front_loading,
     worst_horizon_momentum,
     unanimous_trend_lottery_pick,
-    stochastic_half_upgrade,
-    uptime_momentum,
     winner_tightness_regime_switch,
 ] as const satisfies readonly PairSelectionRule[];
 
